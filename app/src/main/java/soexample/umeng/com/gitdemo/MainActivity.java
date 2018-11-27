6695c2c2a97d8ef3667608246aac86a61dfd538c
17600191528 <1440666301@qq.com>
2018-11-27 15:13:49 +0800

    ont time

---
+++ app/src/main/java/soexample/umeng/com/gitdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package soexample.umeng.com.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //这是wyj第一次提交
+    }
+}
